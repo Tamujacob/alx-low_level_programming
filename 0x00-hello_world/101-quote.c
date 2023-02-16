5fa61204ba1ec8850e84190b1fe9d6e18e7fc16c
TAMUKEDDEJACOB <jacobtamukedde@gmail.com>
2023-02-16 06:53:57 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints "A programm that prints a standard erro"
+ * Return: Always 1 (Success)
+ */
+int main(void)
+
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\r", stdout);
+       return (1);
+}       
