8b0423a05f494ed603fc989f6dcd60f45c69bb88
TAMUKEDDEJACOB <jacobtamukedde@gmail.com>
2023-02-17 05:34:41 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Write a program that prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d ! = '9')
+		{
+		putcha(',');
+		putchar;('');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
