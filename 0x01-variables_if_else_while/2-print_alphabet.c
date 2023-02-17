7a3a7d7dc0ea8cbf64b774e141b9313c81410542
TAMUKEDDEJACOB <jacobtamukedde@gmail.com>
2023-02-17 04:28:51 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabetic letters in lower case
+ * Return: ALways (Succes)
+ */
+
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+
+	putchar(c);
+	putchar('\n')
+		return (0);
+}
