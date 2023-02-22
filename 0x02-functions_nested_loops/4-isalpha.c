c54a130b655c4d9240f6ab2b1e3620b7e89f1f67
TAMUKEDDEJACOB <jacobtamukedde@gmail.com>
2023-02-22 11:04:52 -0800

    5-sign.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,17 @@
 #include "main.h"
-  
+
 /**
  * _isalpha - Checks for alphabetic characters
- * @c: The character to be checcked
- * Return: 1 for alphabetic characters or 0 for anything else
+ * @c: The character to be checked
+ * Description: Prints alphabetic characters
+ * Return; 1 for alphabetic characters or 0 for anything else
  */
 
 int _isalpha(int c)
 {
-        if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-        {
-                return (1);
-        }
-        return (0);
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,24 +3,24 @@
 /**
  * print_sign - Prints the sign of a number
  * @n: The number to be checked
- * Return: 1 for positive number, -1 for negativve number and 0 for anything else
+ * Return: 1 for positive number, -1 for negative number and 0 for anything else
  */
 
 int print_sign(int n)
 {
-        if (n > 0)
-        {
-                _putchar(43);
-                return (1);
-        }
-        else if (n < 0)
-                {
-                _putchar (45);
-                return (-1);
-                }
-        else
-        {
-                _putchar(48);
-                return (0);
-        }
+	if (n > 0)
+	{
+		_putchar (43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar (45);
+		return (-1);
+	}
+	else
+	{
+		_putchar (48);
+		return (0);
+	}
 }
