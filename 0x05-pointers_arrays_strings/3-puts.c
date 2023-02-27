7658488323f9aa4d01455112f427ac2dd295f992
TAMUKEDDEJACOB <jacobtamukedde@gmail.com>
2023-02-27 03:54:26 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  * _puts - Prints a string
  * @str: The string to print
  * Return: void
  */
 
-void _puts(char *str)
+void  _puts(char *str)
 {
 	for (; *str != '\0'; str++)
 	{
