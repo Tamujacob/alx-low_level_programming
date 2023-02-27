521e3d119e3d2411fe768cf6de33a61c3a1c8b8a
TAMUKEDDEJACOB <jacobtamukedde@gmail.com>
2023-02-27 04:45:47 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - Prints every other character of a string
+ * @str: The string to be treated
+ * Return: void
+ */
+
+void puts(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[jr] != '\0')
+	{
+		j++;
+	}
+	for (i = 0;i < j +=2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
