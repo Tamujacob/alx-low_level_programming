c78e2046f0a915c97ab3f06fc97ad0560150038b
TAMUKEDDEJACOB <jacobtamukedde@gmail.com>
2023-03-13 05:13:03 -0700

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/11-atoi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int _atio(char *s)
+{       
+        return (c >= 'a' && c <= 'z');
+}       
+~   
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+my read me file of project 0x09. C - Static libraries
